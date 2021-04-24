12331a88d65a9789c484cdeb09765a40c5629f8c
MADARA19122006 <nikolayb1206@gmail.com>
2021-04-25 01:19:42 +0300

    API availability load added

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 from requests import post
 
+
 print(post('http://localhost:5000/api/availability',
-           json={'date': '20210503', 'room': 1, 'quantity_rooms': 4,
-                 'price': 3000}).json())
+           json={'availability': [{'date': '20210503', 'room': 1, 'qty': 4,
+                 'price': 3000}]}).json())
